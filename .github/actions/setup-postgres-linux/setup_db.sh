9f706c1a886c3ad64e052afa2db815aeb6bf34a7
Mike Alfare <mike.alfare@dbtlabs.com>
2024-01-29 15:41:50 -0500

    revert to core process

--- .github/actions/setup-postgres-linux/setup_db.sh
+++ .github/actions/setup-postgres-linux/setup_db.sh
@@ -11,10 +11,29 @@ PGPORT="${PGPORT:-5432}"
 export PGPORT
 PGHOST="${PGHOST:-localhost}"
 
+function connect_circle() {
+	# try to handle circleci/docker oddness
+	let rc=1
+	while [[ $rc -eq 1 ]]; do
+		nc -z ${PGHOST} ${PGPORT}
+		let rc=$?
+	done
+	if [[ $rc -ne 0 ]]; then
+		echo "Fatal: Could not connect to $PGHOST"
+		exit 1
+	fi
+}
+
+# appveyor doesn't have 'nc', but it also doesn't have these issues
+if [[ -n $CIRCLECI ]]; then
+	connect_circle
+fi
+
 for i in {1..10}; do
 	if pg_isready -h "${PGHOST}" -p "${PGPORT}" -U "${PGUSER}" ; then
 		break
 	fi
+
     echo "Waiting for postgres to be ready..."
     sleep 2;
 done;
