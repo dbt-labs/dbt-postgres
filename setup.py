df1754aeca2d3b4718a9e8aa02534eb5bc5c0124
EitanKatsightfull <121748185+EitanKatsightfull@users.noreply.github.com>
2024-03-17 14:04:45 +0200

    version bump to a bugfixed version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def _dbt_psycopg2_name():
 
 
 package_name = "dbt-postgres"
-package_version = "1.7.9"
+package_version = "1.7.10"
 description = """The postgres adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
