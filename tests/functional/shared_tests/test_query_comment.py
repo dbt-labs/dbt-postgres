09a200f219653e78a12cc4686299a59ddb313084
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-03 15:38:23 -0400

    skip inapplicable test

--- tests/functional/shared_tests/test_query_comment.py
+++ tests/functional/shared_tests/test_query_comment.py
@@ -18,7 +18,7 @@ class TestMacroQueryComments(BaseMacroQueryComments):
 
 
 class TestMacroArgsQueryComments(BaseMacroArgsQueryComments):
-    @pytest.skip(
+    @pytest.mark.skip(
         "This test is incorrectly comparing the version of `dbt-core`"
         "to the version of `dbt-postgres`, which is not always the same."
     )
