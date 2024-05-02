5be5dba55cb0296a671534913fa9973124a65ab2
Mike Alfare <mike.alfare@dbtlabs.com>
2024-05-02 10:27:14 -0400

    clean up test script

--- docker/test.sh
+++ docker/test.sh
@@ -1,22 +1,19 @@
 # - VERY rudimentary test script to run latest + specific branch image builds and test them all by running `--version`
 # TODO: create a real test suite
+set -e
 
-clear \
-&& echo "\n\n"\
-"########################################\n"\
-"##### Testing dbt-postgres latest #####\n"\
-"########################################\n"\
-&& docker build --tag dbt-postgres \
-  --target dbt-postgres \
-  docker \
-&& docker run dbt-postgres --version \
-\
-&& echo "\n\n"\
-"#########################################\n"\
-"##### Testing dbt-postgres-1.0.0b1 #####\n"\
-"#########################################\n"\
-&& docker build --tag dbt-postgres-1.0.0b1 \
-  --target dbt-postgres \
-  --build-arg commit_ref=v1.0.0b1 \
-  docker \
-&& docker run dbt-postgres-1.0.0b1 --version
+echo "\n\n"
+echo "#######################################"
+echo "##### Testing dbt-postgres latest #####"
+echo "#######################################"
+
+docker build --tag dbt-postgres --target dbt-postgres docker
+docker run dbt-postgres --version
+
+echo "\n\n"
+echo "########################################"
+echo "##### Testing dbt-postgres-1.0.0b1 #####"
+echo "########################################"
+
+docker build --tag dbt-postgres-1.0.0b1 --target dbt-postgres --build-arg commit_ref=v1.0.0b1 docker
+docker run dbt-postgres-1.0.0b1 --version
