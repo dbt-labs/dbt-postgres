d2fd8f932b029b0d029a2fa043a318219089d78d
Mike Alfare <mike.alfare@dbtlabs.com>
2024-06-14 13:09:24 -0400

    revert to string parsing to avoid jq dep

--- .github/scripts/psycopg2-check.sh
+++ .github/scripts/psycopg2-check.sh
@@ -3,12 +3,12 @@ source venv/bin/activate
 python -m pip install .
 
 if [[ "$PSYCOPG2_WORKAROUND" == true ]]; then
-    PSYCOPG2_VERSION=$(pip show --format=json | jq -r '.[] | select(.Name == "psycopg2-binary") | .Version')
+    PSYCOPG2_VERSION=$(pip show psycopg2-binary | grep Version | cut -d " " -f 2)
     pip uninstall -y psycopg2-binary
     pip install psycopg2==$PSYCOPG2_VERSION
 fi
 
-PSYCOPG2_NAME=$(pip show --format=json | jq -r '.[] | select(.Name == "psycopg2" or .Name == "psycopg2-binary") | .name')
+PSYCOPG2_NAME=$((pip show psycopg2 || pip show psycopg2-binary) | grep Name | cut -d " " -f 2)
 if [[ "$PSYCOPG2_NAME" != "$PSYCOPG2_EXPECTED_NAME" ]]; then
     echo -e 'Expected: "$PSYCOPG2_EXPECTED_NAME" but found: "$PSYCOPG2_NAME"'
     exit 1
