69cd9d3731866f54d6602aeafb1127999367c14e
Gerda Shank <gerda@dbtlabs.com>
2024-07-29 10:15:43 -0400

    Update script and call with dbt_common

--- .github/scripts/update_dev_dependency_branches.sh
+++ .github/scripts/update_dev_dependency_branches.sh
@@ -2,16 +2,20 @@
 set -e
 
 
-adapters_git_branch=$1
-core_git_branch=$2
+dbt_adapters_branch=$1
+dbt_core_branch=$2
+dbt_common_branch=$3
 target_req_file="pyproject.toml"
-core_req_sed_pattern="s|dbt-core.git.*#subdirectory=core|dbt-core.git@${core_git_branch}#subdirectory=core|g"
-adapters_req_sed_pattern="s|dbt-adapters.git|dbt-adapters.git@${adapters_git_branch}|g"
+core_req_sed_pattern="s|dbt-core.git.*#subdirectory=core|dbt-core.git@${dbt_core_branch}#subdirectory=core|g"
+adapters_req_sed_pattern="s|dbt-adapters.git|dbt-adapters.git@${dbt_adapters_branch}|g"
+common_req_sed_pattern="s|dbt-common.git|dbt-common.git@${dbt_common_branch}|g"
 if [[ "$OSTYPE" == darwin* ]]; then
  # mac ships with a different version of sed that requires a delimiter arg
- sed -i "" "$core_req_sed_pattern" $target_req_file
  sed -i "" "$adapters_req_sed_pattern" $target_req_file
+ sed -i "" "$core_req_sed_pattern" $target_req_file
+ sed -i "" "$common_req_sed_pattern" $target_req_file
 else
- sed -i "$core_req_sed_pattern" $target_req_file
  sed -i "$adapters_req_sed_pattern" $target_req_file
+ sed -i "$core_req_sed_pattern" $target_req_file
+ sed -i "$common_req_sed_pattern" $target_req_file
 fi
